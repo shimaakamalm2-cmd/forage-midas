6fba06438dbdb33a177210f4118fe6e216ffbd63
shimaakamalm2-cmd <shimaakamalm2@gmail.com>
2026-03-24 21:02:42 +0200

    implementer restController to handle a get request for users to view their balance . project done!

--- src/main/java/com/jpmc/midascore/MidasController.java
+++ src/main/java/com/jpmc/midascore/MidasController.java
@@ -1,4 +1,29 @@
 package com.jpmc.midascore;
 
+import com.jpmc.midascore.entity.UserRecord;
+
+import com.jpmc.midascore.foundation.Balance;
+import com.jpmc.midascore.repository.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class MidasController {
+    UserRepository userRepository;
+
+    public MidasController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/balance")
+    public Balance getBalance(@RequestParam Long userId) {
+
+           float amount = userRepository.findById(userId)
+                   .map(UserRecord::getBalance)
+                   .orElse(0.0f);
+        System.out.println((new Balance(amount)).toString());
+           return new Balance(amount);
+    }
 }
